b8cfadff2dc01d180322f9375b45e20978e1dfc9
prenaissance <alexandru.andries@isa.utm.md>
2023-02-08 23:50:08 +0200

    feat(AA): bootrstraped lab1 and shared folder

---
+++ AA/shared/decorators.py
@@ -0,0 +1,27 @@
+import time
+from functools import wraps
+
+
+def benchmark(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        end = time.perf_counter()
+        return {
+            "result": result,
+            "time": end - start
+        }
+    return wrapper
+
+
+def memoize(func):
+    cache = {}
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        key = str(args) + str(kwargs)
+        if key not in cache:
+            cache[key] = func(*args, **kwargs)
+        return cache[key]
+    return wrapper
